0b9eed2372a2a2b8351ca51d413f9220b3729959
atsu666 <ito@appleple.com>
2023-04-19 13:43:56 +0900

    amd64用

---
+++ multi-cpu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# docker build -t atsu666/ioncube:7.4-amd64 . # 各アーキテクチャで実行すること
+
+docker manifest create atsu666/ioncube:7.4 \
+    atsu666/ioncube:7.4-amd64 \
+    atsu666/ioncube:7.4-arm64
+
+docker manifest push atsu666/ioncube:7.4
