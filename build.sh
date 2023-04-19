4148634c8ea0ed5f33ddb6975f6d816bc796770f
atsu666 <ito@appleple.com>
2023-04-19 12:29:31 +0900

    マルチステージビルドのスクリプトを追加

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx ls
+docker buildx build --platform linux/amd64,linux/arm64 -t atsu666/ioncube:7.3 --push .
